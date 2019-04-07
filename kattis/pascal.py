3eb31bd403a1fe5f8bb2b6388ad33067a9af13c3
0xDepp <a@xsq.pw>
2019-04-07 03:25:12 +0100

    Haha Pascal

--- kattis/pascal.py
+++ kattis/pascal.py
@@ -1,1 +1,1 @@
-print(int((lambda x:((lambda y,n:y[0]if len(y)>0 else n-1)(list(filter(lambda x:x!="",[x-x/i if x%i==0 else""for i in range(2,int(x**0.5)+1)])),x)))(int(input()))))
+print(int((lambda x:((lambda y,n:y[0]if len(y)>0 else n-1)(list(filter(lambda x:x!='',[x-x/i if x%i==0 else""for i in range(2,int(x**0.5)+1)])),x)))(int(input()))))
