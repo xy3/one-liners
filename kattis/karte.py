811622b20d4fc82c1a8736e453f366281d04f6c2
0xDepp <a@xsq.pw>
2019-04-16 03:18:21 +0100

    Karte

---
+++ kattis/karte.py
@@ -0,0 +1,1 @@
+(lambda v:([print(13-len(v[z]),end=' ')for z in'PKHT'],print()))((lambda s,v:v if[(lambda f:(lambda:(print('GRESKA'),(__import__('sys')).exit()))()if f in v[s[i]]else v[s[i]].add(f))(s[i+1:i+3])for i in range(0,len(s),3)]else 0)(input(),(lambda v:v if[v.__setitem__(z,set())for z in 'PKHT']else 0)({})))
