6679768259eccee964beed6c7fe138ec748357c3
0xDepp <a@xsq.pw>
2019-04-22 03:42:17 +0100

    music

---
+++ kattis/musicalscales.py
@@ -0,0 +1,1 @@
+(lambda vs,ss:([ss.__setitem__(vs[k],set((lambda v:v if[(lambda:(v.__setitem__(1,(v[1]+x)%12),v[0].append(vs[v[1]])))()for x in[2,2,1,2,2,2,1]]else 0)([[vs[k]],k])[0]))for k in range(12)],(lambda x:print('none'if not len(x)else' '.join(x)))((lambda _,s:[v for v in vs if all(c in ss[v]for c in s)])(input(),input().split()))))('A A# B C C# D D# E F F# G G#'.split(),{})
